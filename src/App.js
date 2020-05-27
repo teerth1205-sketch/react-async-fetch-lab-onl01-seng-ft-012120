ca306f5c547fb7e7945a691682e5502b62236a01
EC2 Default User <ec2-user@ip-172-31-36-178.us-east-2.compute.internal>
2020-05-27 21:40:14 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, {Component} from 'react'
+
+export default class App extends Component {
+    state = {
+
+    }
+ 
+ componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+            .then(response => {
+                return response.json()
+            })
+            .then(json => {
+                console.log(json)
+            })
+    }   
+    
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
